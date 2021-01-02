abe26a8291fbfd6464781e516aa40ff22ae6b1b6
Hoc Can Huynh <canhuynh1998@gmail.com>
2021-01-01 20:52:32 -0800

    Insert all States and Counties

--- Database.py
+++ Database.py
@@ -6,9 +6,11 @@ We are not going to create many instance of different database so everything wil
 import pymongo 
 from pymongo import MongoClient
 from Scrapper import Scrapper
-
+import time
+start_time = time.time()
 class Database:
-    CONNECTION = "mongodb+srv://canhuynh:can1234@cluster0.dehxw.mongodb.net/Covid19?retryWrites=true&w=majority"
+    
+    CONNECTION = "mongodb+srv://canhuynh:<password>@cluster0.dehxw.mongodb.net/<dbname>?retryWrites=true&w=majority"
     DATABASE = None
 
     @staticmethod
@@ -19,23 +21,49 @@ class Database:
     @staticmethod
     def insert(collection, data):
         Database.DATABASE[collection].insert_one(data)
-    
-    @staticmethod
-    def find(collection, data):
-        return Database.DATABASE[collection].find(data)
 
     @staticmethod
     def find_one(collection, data):
         return Database.DATABASE[collection].find_one(data)
+    
+    @staticmethod
+    def update(collection, data):
+        def updateHelper(data, key, existing):
+            if key == "newCases":
+                return data["totalCases"] - existing["totalCases"]
+            else:
+                return data["totalDeath"] - existing["totalDeath"]
+                
+        currentCollection = Database.DATABASE[collection]
+        collectionLength = len(list(currentCollection.find()))
+        index = 0
+        while index < collectionLength:
+            existing = currentCollection.find_one({'_id': index})
+            currentValue = data[index]
+            for key, value in currentValue.items():
+                if key == "newCases" or key == "newDeaths":
+                    value = updateHelper(currentValue, key, existing)
+                currentCollection.update_one({'_id':index},{"$set":{key:value}})
+            index += 1
+
+
+
 
 
 Database.initialize()
 
 # Insert 
-# scrapper = Scrapper()
-# states = scrapper.USA
-
-# for state in states:
-#     Database.insert('States', state)
-
-Database.DATABASE['States'].delete_many({})
+scrapper = Scrapper()
+# states = scrapper.states
+# #print(states)
+# counties = scrapper.counties
+#print(counties)
+# Database.DATABASE['Counties'].delete_many({})
+# Database.DATABASE['States'].delete_many({})
+for county in scrapper.counties:
+    print(county)
+    Database.insert('Counties', county)
+for state in scrapper.states:
+    Database.insert('States',state)
+#Database.update('States', states)
+print("--- %s seconds ---" % (time.time() - start_time))
