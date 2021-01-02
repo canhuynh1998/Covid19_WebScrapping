aa11a44fb2d8a2b132910d8a51b70cf55c8f04f7
Hoc Can Huynh <canhuynh1998@gmail.com>
2021-01-01 20:49:52 -0800

    Final commit

--- Scrapper.py
+++ Scrapper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 global statelookup
 statelookup = [
     ("california","usa/california/"), ("texas" ,"usa/texas/"), ("florida", 'usa/florida/'), ("illinois" ,"usa/illinois/"),
@@ -12,15 +13,19 @@ statelookup = [
     ('south-carolina','usa/south-carolina/'), ('iowa','usa/iowa/'), ('oklahoma','usa/oklahoma/'),('maryland','usa/maryland/'),
     ('utah','usa/utah/'), ('kentucky','usa/kentucky/'), ('washington','usa/washington/'),('nevada','usa/nevada/') ,('arkansas','usa/arkansas/'),
     ('kansas','usa/kansas/'), ('mississippi','usa/mississippi/'), ('connecticut','usa/connecticut/'), ('nebraska','usa/nebraska/') , ('new-mexico','usa/new-mexico/'),
-    ('',''),
+    ('idaho','usa/idaho/'),('oregon','usa/oregon/'), ('south-dakota','usa/south-dakota/'), ('north-dakota','usa/north-dakota/'), ('rhode-island','usa/rhode-island/'),
+    ('west-virginia','usa/west-virginia/'),('montana','usa/montana/'), ('delaware','usa/delaware/'),('alaska','usa/alaska/'), ('wyoming','usa/wyoming/'),
+    ('usa/new-hampshire/','new-hampshire'),('district-of-columbia','usa/district-of-columbia/') ,('maine','usa/maine/'), ('hawaii','usa/hawaii/'),
+    ('vermont','usa/vermont/')
 ]
 
 class Scrapper:
     
     def __init__(self):
-        
-        #self.counties = self.getCounties()
-        self.USA = self._formatUSA()
+        self.counties = self.formatCounties()
+        #print(self.counties)
+        self.states = self.formatStates()
+        print('Everything is ready!!')
         
     
     def scrap(self, isUSA, state=''):
@@ -29,40 +34,48 @@ class Scrapper:
             URL += 'country/us'
         else:
             URL += state
-            print(URL)
+            #print(URL)
         html = requests.get(URL)
-       # print(state)
-        soup = BeautifulSoup(html.text, 'lxml')
-                                            # id="usa_table_countries_today"
+        soup = BeautifulSoup(html.text, 'lxml')                                       
         table = soup.find("table", attrs={"id" : "usa_table_countries_today"})
-        # print(table)
-        # # if not table.tbody():
-        # #     return
         if table is None:
-            print('There is no available information!')
+            #print('There is no available information!')
             return []
         head = table.tbody.find_all("tr")
         return head
     
 
     def getCounties(self):
+        '''
+        Scrapping data publicly available for all counties of USA
+        Traverse through the statelookup global list to access URL for each state.
+        Return a list of tuple as tuple will protect data from accidentally modifying 
+        Each tuple contains important data for each state.
+        (Name, Total Cases, New Cases, Total Death, New Death, Active Case )
+        '''
         headings = []
+        print('Generating data')
+        states = 0
         for idx in range(len(statelookup)):
             county = statelookup[idx][1]
             head = self.scrap(False, county)
-            
-
-            for i in range(len(head)):
-                #print('Hi')
+            state = statelookup[idx][0]
+            if '-' in state:
+                state = state.replace('-',' ')
+            state = state.title()
+            states += 1
+            for i in range(1,len(head)):
                 row = []
                 td = head[i].find_all('td')
+                #print(td[0].text.replace('\n','').strip())
                 for i, th in enumerate(td):
                     if i == 6:
                         break
                     value = th.text.replace('\n','').strip()
-                    #print(value)
                     if value == '' or value == 'N/A':
-                        value = 0
+                        value = 0   #Default Value
+                    
+                    #This block is to convert all numeric data from String into integer
                     if i != 0 and type(value) is str:
                         letter = ''
                         for char in value:
@@ -75,16 +88,21 @@ class Scrapper:
                             letter = letter.strip()
                         value = float(letter)
                     row.append(value)
+                row.append(state)
                 headings.append(tuple(row))
-        print(len(headings))
-        print(headings)
         return headings
 
     
     def getUSA(self):
+        '''
+        Scrapping data for all states of USA
+        Return a list of tuple as tuple will protect data from accidentally modifying 
+        Each tuple contains important data for each state.
+        (Name, Total Cases, New Cases, Total Death, New Death, Active Case )
+        '''
         head = self.scrap(True)
         headings= []
-
+        print('Loading')
         for i in range(len(head)):
             row = []
             td = head[i].find_all('td') #find all td tags
@@ -93,10 +111,12 @@ class Scrapper:
                     break
                 if i == 6:
                     continue
-                value = th.text.replace('\n','').strip() #elimiate all the white space
+                value = th.text.replace('\n','').strip() #eliminate all the white space
                 if value == "" or value == 'N/A':
-                    value = 0
-                if i != 1 and type(value) is str:
+                    value = 0   # Default Value
+                
+                #This block is to convert all numeric data from String into integer
+                if i != 1 and type(value) is str:   
                     #this is for general useage
                         letter = ''
                         for char in value:
@@ -109,17 +129,18 @@ class Scrapper:
                             letter = letter.strip()
                         value = float(letter)
                 row.append(value)
-            headings.append(tuple(row))
-        # print(len(headings))
-        # print(headings)
+            headings.append(tuple(row)) 
         return headings
 
 
-    def _formatUSA(self):
+    def formatStates(self):
         '''
-        Reformat the instance list self.USA to have the same format with MongoDB
+        Reformat the instance list self.states to have the same format with MongoDB
+        Return a list of dictionaries since MongoDB requires dictionary formatted
+        Keys for these dictionaries are also keys for searching in MongoDB
         '''
         results = []
+        print('Generating data')
         for state in self.getUSA():
             result = {  "_id":None,
                         "states":None,
@@ -130,6 +151,7 @@ class Scrapper:
                         "activeCases":None 
                         }
             i = 0
+            #Assign the correct value to the correct key
             while i < len(state):
                 if i == 0:
                     result['_id'] = int(state[i])
@@ -147,11 +169,50 @@ class Scrapper:
                     result["activeCases"] = state[i]
                 i += 1
             results.append(result)
-        # print(len(results))
-        # print(results)
+        #print(results)
         return results
 
-scrapper = Scrapper()
-scrapper._formatUSA()
-    
+    def formatCounties(self):
+        '''
+        Reformat the instance list self.states to have the same format with MongoDB
+        Return a list of dictionaries since MongoDB requires dictionary formatted
+        Keys for these dictionaries are also keys for searching in MongoDB
+        '''
+        results = []
+        print('Loading')
+        for index, state in enumerate(self.getCounties()):
+            result = {  "_id":index,
+                        "county":None,
+                        "state" : None,
+                        "totalCases":None,
+                        "newCases": None,
+                        "totalDeath":None,
+                        "newDeaths":None,
+                        "activeCases":None 
+                        }
+            i = 0
+            #Assign the correct value to the correct key
+            while i < len(state):
+                if i == 0:
+                    result['county'] = state[i]
+                elif i == 1:
+                    result['totalCases'] = state[i]
+                elif i == 2:
+                    result["newCases"] = state[i]
+                elif i == 3:
+                    result["totalDeath"] = state[i]
+                elif i == 4:
+                    result["newDeaths"] =state[i]
+                elif i == 5:
+                    result["activeCases"] = state[i]
+                else:
+                    result["state"] = state[i]
+                i += 1
+            results.append(result)
+      
+        return results
+
+#scrapper = Scrapper()
+#print(len(statelookup))
+#print("--- %s seconds ---" % (time.time() - start_time))
     
